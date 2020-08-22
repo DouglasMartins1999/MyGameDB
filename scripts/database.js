b8774b53eb422e89bf385061693b814aed16a75b
Douglas Martins <douglas.martins.m099@gmail.com>
2020-08-22 16:00:43 -0300

    Add Postgres to Production

--- scripts/database.js
+++ scripts/database.js
@@ -1,10 +1,16 @@
 const Knex = require("knex");
-const knex = Knex({
-    client: "sqlite3",
-    connection: {
-        filename: "./db.sqlite",
-    },
-    useNullAsDefault: true
-})
+const settings = {};
 
-module.exports = knex;
+if(process.env.NODE_ENV === 'production'){
+    settings.client = "pg";
+    settings.connection = process.env.DATABASE_URL
+    
+} else {
+    settings.useNullAsDefault = true
+    settings.client = "sqlite3"
+    settings.connection = {
+        filename: "./db.sqlite",
+    }
+}
+
+module.exports = Knex(settings);
